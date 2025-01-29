40c449a76f05ce904f882e58be9f90e674b75234
Vercel Security testing <118594933+w2h3@users.noreply.github.com>
2025-01-29 16:34:47 +0200

    Update route.js

--- app/api/redirect/route.js
+++ app/api/redirect/route.js
@@ -1,4 +1,7 @@
 export async function GET(req) {
-    const { searchParams } = new URL(req.url);
-    return Response.redirect(new URL(searchParams.get("url")).toString(), 302);
+    const { pathname, searchParams } = new URL(req.url);
+    let target = searchParams.get("url") || pathname.replace(/^\/api\/redirect\//, "");
+    return Response.redirect(target, 302);
+    const domain = req.nextUrl.pathname.replace(/^\/api\/redirect\//, "");
+    return Response.redirect(`https://${domain}`, 302);
 }
