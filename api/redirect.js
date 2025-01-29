0ebba836fd5ce61078e3b0c959ff1c20882406b7
Vercel Security testing <118594933+w2h3@users.noreply.github.com>
2025-01-29 17:09:27 +0200

    Create redirect.js

---
+++ api/redirect.js
@@ -0,0 +1,15 @@
+export default function handler(req, res) {
+  const { protocol, path } = req.query;
+  if (!protocol || !path) {
+    res.status(400).send("Invalid request");
+    return;
+  }
+  let redirectUrl;
+  if (protocol === "file") {
+    redirectUrl = `file:///${path}`;
+  } else {
+    redirectUrl = `${protocol}://${path}`;
+  }
+  res.writeHead(307, { Location: redirectUrl });
+  res.end();
+}
