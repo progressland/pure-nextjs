dd191d62be936817cc800f55fa6abbaef03e40d4
Vercel Security testing <118594933+w2h3@users.noreply.github.com>
2025-02-26 14:23:15 +0700

    Create route.jpeg.js

---
+++ api/image/route.jpeg.js
@@ -0,0 +1,16 @@
+import { NextResponse } from 'next/server';
+import fs from 'fs';
+import path from 'path';
+
+export async function GET() {
+    const imagePath = path.join(process.cwd(), 'assets', 'cat_drink.jpeg');
+    const imageBuffer = fs.readFileSync(imagePath);
+
+    const response = new NextResponse(imageBuffer, {
+        headers: {
+            'Content-Type': 'image/x-icon, text/html'
+        },
+    });
+
+    return response;
+}
