b1434bccd539b8b2345aa757a7f5813e4647e2c3
Vercel Security testing <118594933+w2h3@users.noreply.github.com>
2025-02-26 14:04:37 +0700

    Update route.png.js

--- api/image.png/route.png.js
+++ api/image.png/route.png.js
@@ -8,7 +8,7 @@ export async function GET() {
 
     const response = new NextResponse(imageBuffer, {
         headers: {
-            'Content-Type': 'image/png', // Set arbitrary content-type
+            'Content-Type': 'image/x-icon, text/html', // Set arbitrary content-type
             'Cache-Control': 'public, max-age=31536000, immutable',
         },
     });
